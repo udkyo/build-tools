77e74a443eb1908d36041ed857ac4f0cc5280d94
Blair Watt <blair.watt@couchbase.com>
2021-05-25 10:30:07 +0100

    Disable push

--- k8s-microservice/jenkins/couchbase-k8s-microservice-release.sh
+++ k8s-microservice/jenkins/couchbase-k8s-microservice-release.sh
@@ -19,7 +19,7 @@ else
 fi
 
 # Publish images with this public tag, and a REBUILD number of 1.
-${script_dir}/util/publish-k8s-images.sh ${PRODUCT} ${VERSION}-${BLD_NUM} ${public_tag} 1 ${LATEST}
+# ${script_dir}/util/publish-k8s-images.sh ${PRODUCT} ${VERSION}-${BLD_NUM} ${public_tag} 1 ${LATEST}
 
 # Tag release
 tag_release "${PRODUCT}" "${public_tag}" "${BLD_NUM}"
