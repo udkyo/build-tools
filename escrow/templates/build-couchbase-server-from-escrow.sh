41babd651f0745f8d9eee08091a385e99a2b2873
udkyo <blair.watt@couchbase.com>
2022-11-08 11:59:50 +0000

    Add in-container user to docker group

--- escrow/templates/build-couchbase-server-from-escrow.sh
+++ escrow/templates/build-couchbase-server-from-escrow.sh
@@ -63,14 +63,24 @@ if [ $? -ne 0 ]
 then
   set -e
   heading "Starting Docker worker container..."
+  # We need to make sure the user inside the container can
+  # access the docker socket for interacting with the sidecar
+  # containers, so we get the docker gid on the host, create
+  # the docker group in the container, and then add the couchbase
+  # user to this group
+  dockergroup=$(getent group docker | cut -d: -f3)
+
   # We specify external DNS (Google's) to ensure we don't find
   # things on our LAN. We also point packages.couchbase.com to
   # a bogus IP to ensure we aren't dependent on existing packages.
   docker run --name "${WORKER}" -d \
     --add-host packages.couchbase.com:8.8.8.8 \
     --dns 8.8.8.8 \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    "${IMAGE}" tail -f /dev/null
+    -v /var/run/docker.sock:/var/run/docker.sock:rw \
+    "${IMAGE}" bash -c "set -x \
+                          && groupadd -g ${dockergroup} docker \
+                          && usermod -aG docker couchbase \
+                          && tail -f /dev/null"
 else
   docker start "${WORKER}"
 fi
